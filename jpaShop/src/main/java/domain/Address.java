4f61eec9d5156fc4774129fa093087ac6fc4beec
hunsu <joranegs92@gmail.com>
2021-04-22 22:40:30 +0900

    일단올림..

---
+++ jpaShop/src/main/java/domain/Address.java
@@ -0,0 +1,11 @@
+package domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
---
+++ jpaShop/src/main/java/domain/Member.java
@@ -0,0 +1,22 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "members")
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    @Embedded
+    private Address address;
+
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+}
