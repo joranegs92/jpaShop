e47972514c27d0cd33f7450272d8f095ce15ab5f
joranegs92 <joranegs92@gmail.com>
2021-08-03 23:00:53 +0900

    front 시작 오류있음

--- jpaShop/src/test/java/jpaTest/jpaShop/domain/MemberRepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package jpaTest.jpaShop.domain;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-}
