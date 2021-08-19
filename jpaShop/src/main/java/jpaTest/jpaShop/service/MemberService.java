1d3346a12edadea017093baa10ffad8c19398811
joranegs92 <joranegs92@gmail.com>
2021-08-19 23:16:34 +0900

    gg

--- jpaShop/src/main/java/jpaTest/jpaShop/service/MemberService.java
+++ jpaShop/src/main/java/jpaTest/jpaShop/service/MemberService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
