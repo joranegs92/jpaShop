6301bcf60b085f2f0aee488d96a0a12acb07f003
joranegs92 <joranegs92@gmail.com>
2021-08-30 22:49:39 +0900

    memberForm 추가

---
+++ jpaShop/src/main/java/jpaTest/jpaShop/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package jpaTest.jpaShop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter @Setter
+public class MemberForm {
+
+
+    private String name;
+    private String city;
+    private String street;
+
+}
